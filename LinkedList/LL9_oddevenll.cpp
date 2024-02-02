145e134001db5330c53448909e1f838a73527e2a
ukpandey <ujjwalpandey223@gmail.com>
2024-02-02 15:56:27 +0530

    group odd positioned and even positioned nodes together

---
+++ LinkedList/LL9_oddevenll.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+    
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+void traverse(Node *head){
+    if(!head) {
+        cout<<"NULL hai head"<<endl; 
+        return;
+    }
+    int len = 0;
+    Node *temp = head;
+    while(temp->next!=NULL){
+        len++;
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }
+    len++;
+    cout<<temp->data;
+    cout<<"\nLength: "<<len<<endl;
+}
+
+Node* convertArrtoLL(vector<int> v){
+    int n = v.size();
+    if(n==0) return NULL;
+    Node* head = new Node(v[0]);
+    Node* temp = head;
+    for(int i=1; i<n; i++){
+        temp->next = new Node(v[i]);
+        temp = temp->next;
+    }
+    return head;
+}
+
+Node* groupOddEven(Node* head){
+    if(!head || !head->next) return head;
+    Node* odd = head;
+    Node* even = head->next;
+    Node* evenHead = even;
+    while(even!=NULL && even->next!=NULL){
+        odd->next = odd->next->next;
+        even->next = even->next->next;
+        odd = odd->next;
+        even = even->next;
+    }
+    odd->next = evenHead;
+    return head;
+}
+
+int main(){
+    Node *head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+    head->next->next->next->next->next = new Node(6);
+    traverse(head);
+    head = groupOddEven(head); // 1 3 5 2 4 6
+    traverse(head);
+
+
+}
